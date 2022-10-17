36a7c0a8e65917a092fbcd4c3b1999001ec3c7f4
Sansone11 <sansonemirco@gmail.com>
2022-10-17 20:07:56 +0200

    confirm-edit

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,44 +3,59 @@
 {{-- @section('mataTitle', 'Comics') --}}
 
 @section('content')
+    <div class="container_show">
+        <h1>{{ $comic->title }}</h1>
+        <div class="description">
+            <ul>
+                <li>
+                    <span>Descrizione</span>
+                    <p>{{ $comic->description }}</p>
+                </li>
+                <li>
+                    <p>{{ $comic->thumb }}</p>
+                </li>
+                <li>
+                    <span>Prezzo</span>
+                    <p>{{ $comic->price }}</p>
+                </li>
+                <li>
+                    <span>Serie</span>{{ $comic->series }}
+                </li>
+                <li>
+                    <span>Data di uscita</span>{{ $comic->sale_date }}
+                </li>
+                <li>
+                    <span>Genere</span> {{ $comic->type }}
+                </li>
+            </ul>
 
-    
-<div class="container_show">
-    <h1>{{$comic->title}}</h1>
-    <div class="description">
-        <ul>
-            <li>
-               <span>Descrizione</span> <p>{{$comic->description}}</p>
-            </li>
-            <li>
-                <p>{{$comic->thumb}}</p>
-            </li>
-            <li>
-               <span>Prezzo</span> <p>{{$comic->price}}</p>
-            </li>
-            <li>
-                <span>Serie</span>{{$comic->series}}
-            </li>
-            <li>
-                <span>Data di uscita</span>{{$comic->sale_date}}
-            </li>
-            <li>
-               <span>Genere</span> {{$comic->type}}
-            </li>
-        </ul>
-        
+        </div>
     </div>
-</div>
-<section>
-    <div>
-       <a href="{{ route('comics.edit',$comic->id) }}">Modifica fumetto</a> 
-       <form action="{{ route('comics.destroy',$comic->id) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <input type="submit" value="Elimina"> 
-       </form>
-       
-    </div>
-</section>
+    <section>
+        <div>
+            <a href="{{ route('comics.edit', $comic->id) }}">Modifica fumetto</a>
+            <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <input onclick="Conferma_eliminazione()" type="submit" value="Elimina">
 
+                <script type="text/javascript">
+                    function Conferma_eliminazione() {
+
+                        var domanda = confirm("Sicuro di eliminaree questo fumetto...?");
+
+                        if (domanda == true) {
+                            alert('fumetto cancellato');
+                            
+                        } else {
+                            alert('Non hai cancellato');
+                        }
+                    }
+                </script>
+
+
+            </form>
+
+        </div>
+    </section>
 @endsection
